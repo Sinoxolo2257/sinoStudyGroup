a07c6c583b41a095948efc0c17c9995c92e87601
u22579428 <u22579428@INFRD-28>
2025-02-28 11:48:46 +0200

    Add project files.

---
+++ sinoStudyGroup/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sinoStudyGroup.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<Models.group> members = new List<Models.group>();
+
+            members.Add(new Models.group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
+            members.Add(new Models.group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
+            members.Add(new Models.group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
+            members.Add(new Models.group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
+            members.Add(new Models.group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
+            return View(members);
+        }
+    }
+}
---
+++ sinoStudyGroup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sinoStudyGroup.MvcApplication" Language="C#" %>
