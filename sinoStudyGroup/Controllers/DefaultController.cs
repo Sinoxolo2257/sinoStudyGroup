58807f89e4e7a1a531ffd467964638e67d8c8d3b
u22579428 <u22579428@tuks.co.za>
2025-03-03 16:58:14 +0200

    updated controller

--- sinoStudyGroup/Controllers/DefaultController.cs
+++ sinoStudyGroup/Controllers/DefaultController.cs
@@ -13,7 +13,7 @@ namespace sinoStudyGroup.Controllers
         private static List<Group> members = new List<Group> //kep list at class level to keep persistance while app is running (list in memory while app is running)
         {
              members.Add(new Models.Group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
-             members.Add(new Models.Group { StudentNo = "u22579428", FirstName = "Naledi", Surname = "Nkwana", Email = "u24687945tuks.co.za" });
+             members.Add(new Models.Group { StudentNo = "u24687945", FirstName = "Naledi", Surname = "Nkwana", Email = "u24687945@tuks.co.za" });
              members.Add(new Models.Group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
              members.Add(new Models.Group { StudentNo = "u22579428", FirstName = "Sinoxolo", Surname = "Sokoyi", Email = "u225794282tuks.co.za" });
         };
